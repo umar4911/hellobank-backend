e40a4e5bb36a055d92a1b59f11ae3492271a191d
umar4911 <umar4911@gmail.com>
2025-12-03 19:55:57 +0500

    feat: added api for user cards

--- src/api/UserRoute.js
+++ src/api/UserRoute.js
@@ -6,6 +6,7 @@ const Middleware = require("../cors/Middleware");
 module.exports = () => {
   router.use(Middleware.UserAuth);
   router.get("/", UserController.GetInfo);
+  router.get("/cards", UserController.GetCards);
   router.patch("/update/email", UserController.UpdateEmail);
   router.patch("/update/password", UserController.UpdatePassword);
 
